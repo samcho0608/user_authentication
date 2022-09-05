c834bce55ae2cd02f9f6257f84927af0716c172d
Sam Cho <samcho9968@gmail.com>
2022-09-05 23:26:50 +0900

    <REFACTOR> Entity들 기본 생성자 추가 및 User 클래스 AppUser로 rename
    DB에 넣을 값을 세팅해줄 수 있게끔 Entity에 기본 생성자 추가함. User라는 클래스 명은 다양한 곳에서 사용되어 중복될 수 있으므로 AppUser로 rename.

--- src/main/kotlin/com/samcho/user_authentication/domain/user/AppUser.kt
+++ src/main/kotlin/com/samcho/user_authentication/domain/user/AppUser.kt
@@ -6,28 +6,28 @@ import com.samcho.user_authentication.domain.user.phone.Phone
 /**
  * 유저 정보 Entity
  */
-class User(
-    var id: String,
+class AppUser(
+    var id: String? = null,
 
     /**
      * 유저 닉네임. Unique
      */
-    var nicknm: String,
+    var nicknm: String = "",
 
     /**
      * 유저 이메일. Unique
      */
-    var email: EmailAddress,
+    var email: EmailAddress? = null,
     /**
      * 유저 이름
      */
-    var name: String,
+    var name: String = "",
     /**
      * 비밀번호. BCrypt로 암호화
      */
-    var password: String,
+    var password: String = "",
     /**
      * 유저 전화번호
      */
-    var phone: Phone,
+    var phone: Phone? = null,
 )
--- src/main/kotlin/com/samcho/user_authentication/domain/user/phone/Phone.kt
+++ src/main/kotlin/com/samcho/user_authentication/domain/user/phone/Phone.kt
@@ -4,15 +4,15 @@ package com.samcho.user_authentication.domain.user.phone
  * 핸드폰 정보 Entity
  */
 class Phone(
-    var id : String,
+    var id : String? = null,
 
     /**
      * 국가번호
      */
-    var countryCode : CountryCode,
+    var countryCode : CountryCode? = null,
 
     /**
      * 가입자 번호
      */
-    var subscriberNumber : SubscriberNumber,
+    var subscriberNumber : SubscriberNumber? = null,
 )
