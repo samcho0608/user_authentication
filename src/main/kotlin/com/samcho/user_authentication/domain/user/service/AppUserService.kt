e09bec6e5c31500c1354f067f9cef644fb461cda
Sam Cho <samcho9968@gmail.com>
2022-09-06 14:55:33 +0900

    <FEAT> AppUserService 클래스의 기능들을 구현함
    유저의 CRUD 기능 관련 처리만을 담당하는 비즈니스 로직을 구현하게끔 구현함.

--- src/main/kotlin/com/samcho/user_authentication/domain/user/service/AppUserService.kt
+++ src/main/kotlin/com/samcho/user_authentication/domain/user/service/AppUserService.kt
@@ -2,25 +2,25 @@ package com.samcho.user_authentication.domain.user.service
 
 import com.samcho.user_authentication.domain.user.AppUser
 import com.samcho.user_authentication.domain.user.AppUserDetail
+import com.samcho.user_authentication.domain.user.UserNotFoundException
 import com.samcho.user_authentication.domain.user.repository.AppUserRepository
 
 class AppUserService(
     private val appUserRepository: AppUserRepository
 ) {
+    fun findUserDetail(user: AppUser): AppUserDetail =
+        appUserRepository.findAppUserDetailById(user.id!!) ?: throw UserNotFoundException()
 
-    fun findUserDetail(user: AppUser): AppUserDetail? {
-        TODO("Not yet implemented")
-    }
+    fun findUser(user: AppUser): AppUser =
+        appUserRepository.findById(user.id!!) ?: throw UserNotFoundException()
 
-    fun findUser(user: AppUser): AppUser? {
-        TODO("Not yet implemented")
-    }
-
-    fun signUp(user: AppUser): AppUser {
-        TODO("Not yet implemented")
-    }
+    fun signUp(user: AppUser): AppUser =
+        appUserRepository.save(user)
 
     fun resetPassword(user: AppUser, newPassword: String) {
-        TODO("Not yet implemented")
+        if(!appUserRepository.existsById(user.id!!)) {
+            throw UserNotFoundException()
+        }
+        appUserRepository.updatePasswordById(user.id!!, newPassword)
     }
 }
