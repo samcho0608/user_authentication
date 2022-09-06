65b24da86900cada3a07b47984a99fe0371871ad
Sam Cho <samcho9968@gmail.com>
2022-09-06 22:22:12 +0900

    <FEAT> 인증번호 형식에 대한 self-validation 로직 구현하여 VerificationCode에 적용

--- src/main/kotlin/com/samcho/user_authentication/domain/verification/verification_code/VerificationCode.kt
+++ src/main/kotlin/com/samcho/user_authentication/domain/verification/verification_code/VerificationCode.kt
@@ -2,4 +2,10 @@ package com.samcho.user_authentication.domain.verification.verification_code
 
 data class VerificationCode(
     val code : String,
-)
+) {
+    init {
+        if(!Regex("^\\d{6}$").matches(code)) {
+            throw InvalidVerificationCodeException()
+        }
+    }
+}
