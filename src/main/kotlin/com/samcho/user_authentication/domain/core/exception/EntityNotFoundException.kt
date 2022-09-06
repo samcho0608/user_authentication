eddb937522ec379994c2084f90a17f678db992c3
Sam Cho <samcho9968@gmail.com>
2022-09-06 17:40:59 +0900

    <DOC> EntityNotFoundException 클래스에 설명 작성

--- src/main/kotlin/com/samcho/user_authentication/domain/core/exception/EntityNotFoundException.kt
+++ src/main/kotlin/com/samcho/user_authentication/domain/core/exception/EntityNotFoundException.kt
@@ -1,5 +1,8 @@
 package com.samcho.user_authentication.domain.core.exception
 
+/**
+ * 데이터 원천에 검색한 Entity가 없을 경우 발생하는 예외
+ */
 open class EntityNotFoundException(
     entityName: String,
 ) : Exception("$entityName not found")
