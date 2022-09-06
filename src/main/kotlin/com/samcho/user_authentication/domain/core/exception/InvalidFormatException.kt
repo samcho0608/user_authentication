d90bd441a4510e9a257223220ba773a8c6782032
Sam Cho <samcho9968@gmail.com>
2022-09-06 15:23:39 +0900

    <REFACTOR> 프로젝트내 패키지 이름 convention을 유지하기 위해 exceptions를 exception으로 Rename

--- src/main/kotlin/com/samcho/user_authentication/domain/core/exception/InvalidFormatException.kt
+++ src/main/kotlin/com/samcho/user_authentication/domain/core/exception/InvalidFormatException.kt
@@ -1,4 +1,4 @@
-package com.samcho.user_authentication.domain.core.exceptions
+package com.samcho.user_authentication.domain.core.exception
 
 /**
  * 처리하려는 형식이 정해진 종류의 데이터 input 값이 맞지 않을 경우 발생하는 예외
--- src/main/kotlin/com/samcho/user_authentication/domain/user/phone_number/InvalidPhoneNumberFormatException.kt
+++ src/main/kotlin/com/samcho/user_authentication/domain/user/phone_number/InvalidPhoneNumberFormatException.kt
@@ -1,6 +1,6 @@
 package com.samcho.user_authentication.domain.user.phone_number
 
-import com.samcho.user_authentication.domain.core.exceptions.InvalidFormatException
+import com.samcho.user_authentication.domain.core.exception.InvalidFormatException
 
 /**
  * 전화번호 값의 형식에 맞지 않는 값이 전달됐을 경우 발생하는 예외
