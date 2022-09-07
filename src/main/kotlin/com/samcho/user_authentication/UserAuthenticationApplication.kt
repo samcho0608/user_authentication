8d6cd0fd37416e7b806b88e645ff29c7b54e4257
Sam Cho <samcho9968@gmail.com>
2022-09-08 04:42:18 +0900

    <CHORE> 서버 구동 시 Garbage 데이터를 넣는 함수 제거

--- src/main/kotlin/com/samcho/user_authentication/UserAuthenticationApplication.kt
+++ src/main/kotlin/com/samcho/user_authentication/UserAuthenticationApplication.kt
@@ -1,31 +1,10 @@
 package com.samcho.user_authentication
 
-import com.samcho.user_authentication.domain.user.AppUser
-import com.samcho.user_authentication.domain.user.email_address.EmailAddress
-import com.samcho.user_authentication.domain.user.phone_number.PhoneNumber
-import com.samcho.user_authentication.domain.user.service.AppUserService
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class UserAuthenticationApplication {
-	@Bean
-	fun run(appUserService: AppUserService) : CommandLineRunner {
-		return CommandLineRunner {
-			appUserService.signUp(
-				AppUser().apply {
-					nicknm = "Sam"
-					name = "Sam"
-					email = EmailAddress("organicyellow0608@gmail.com")
-					password = "Yearbook101!"
-					phoneNumber = PhoneNumber("821084273267")
-				}
-			)
-		}
-	}
-}
+class UserAuthenticationApplication
 
 fun main(args: Array<String>) {
 	runApplication<UserAuthenticationApplication>(*args)
