12af192768f3029c98f9623188476f7e85f5d901
Sam Cho <samcho9968@gmail.com>
2022-09-07 11:58:54 +0900

    <FEAT> VerificationService의 비즈니스 로직을 구현하여 함수 정의

--- src/main/kotlin/com/samcho/user_authentication/domain/verification/VerificationService.kt
+++ src/main/kotlin/com/samcho/user_authentication/domain/verification/VerificationService.kt
@@ -9,10 +9,20 @@ class VerificationService @Autowired constructor(
 ){
 
     fun verifyVerification(verification : Verification) {
-        TODO("Not yet implemented")
+        val foundVerification = verificationRepository.findById(verification.verificationChannel)
+            ?: throw VerificationFailureException("Verification not found")
+
+        if(verification.verificationCode.code != foundVerification.verificationCode.code) {
+            throw VerificationFailureException("Verification code mismatch")
+        }
+
+        if(foundVerification.isExpired()) {
+            throw VerificationFailureException("Verification expired")
+        }
+
+        verificationRepository.delete(foundVerification)
     }
 
-    fun createVerification(verification : Verification) : Verification {
-        TODO("Not yet implemented")
-    }
+    fun createVerification(verification : Verification) : Verification =
+        verificationRepository.save(verification)
 }
