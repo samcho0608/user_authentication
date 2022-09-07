10907094ec9afa96f5b42953e7f26fe5581108a6
Sam Cho <samcho9968@gmail.com>
2022-09-08 00:06:44 +0900

    <REFACTOR> ApiRoute 추상 클래스를 통해 한 곳에서 routing 정보 관리의 편의성을 높힘

--- src/main/kotlin/com/samcho/user_authentication/presentation/verification/VerificationController.kt
+++ src/main/kotlin/com/samcho/user_authentication/presentation/verification/VerificationController.kt
@@ -10,28 +10,27 @@ import com.samcho.user_authentication.domain.verification.sms.SmsService
 import com.samcho.user_authentication.domain.verification.verification_code.VerificationCode
 import com.samcho.user_authentication.domain.verification.verification_code.VerificationCodeGenerator
 import com.samcho.user_authentication.domain.core.util.logger
+import com.samcho.user_authentication.presentation.ApiRoute
 import com.samcho.user_authentication.presentation.dto.ErrorResponseBody
 import com.samcho.user_authentication.presentation.dto.SuccessResponseBody
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.time.Instant
 import java.time.temporal.ChronoUnit
 import java.util.*
 
 @RestController
-@RequestMapping("/verifications")
 class VerificationController @Autowired constructor(
     private val verificationService: VerificationService,
     private val smsService: SmsService,
     private val jwtFactory: JwtFactory
 ) {
 
-    @PostMapping("/phones")
-    fun sendVerification(@RequestBody request: PhoneVerificationRequest): ResponseEntity<Any> {
+    @PostMapping(ApiRoute.PHONE_VERIFICATIONS)
+    fun sendPhoneVerification(@RequestBody request: PhoneVerificationRequest): ResponseEntity<Any> {
         val verification = verificationService.createVerification(
             Verification().apply {
                 verificationCode = VerificationCodeGenerator.generateVerificationCode()
@@ -57,7 +56,7 @@ class VerificationController @Autowired constructor(
         }
     }
 
-    @PostMapping("phones/verify")
+    @PostMapping(ApiRoute.VERIFY_PHONE_VERIFICATIONS)
     fun verifyPhoneVerification(@RequestBody request: VerifyPhoneVerificationRequest): ResponseEntity<Any>  {
         return try {
             val phoneNumber = request.run { countryCode + phoneNumber }
