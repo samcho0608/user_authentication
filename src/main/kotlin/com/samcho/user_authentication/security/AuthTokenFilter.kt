5633d25d619447b6ea0f3b2e1c6e58d34135459d
Sam Cho <samcho9968@gmail.com>
2022-09-08 01:22:21 +0900

    <FIX> 회원가입 요청에 전화번호 인증을 체크하지 않는 오류 수정

--- src/main/kotlin/com/samcho/user_authentication/security/AuthTokenFilter.kt
+++ src/main/kotlin/com/samcho/user_authentication/security/AuthTokenFilter.kt
@@ -30,17 +30,19 @@ class AuthTokenFilter(
         val allPermittedRoutes = listOf(
             // 유저 Collection 경로
             ApiRoute.LOG_IN,
-            ApiRoute.USERS,
 
             // Verification Collection 경로
             ApiRoute.VERIFICATIONS,
             ApiRoute.PHONE_VERIFICATIONS,
             ApiRoute.VERIFY_PHONE_VERIFICATIONS
         )
+
         if(request.servletPath in allPermittedRoutes) {
             filterChain.doFilter(request, response)
+        } else if (request.servletPath == ApiRoute.USERS && request.method == "POST") {
+            TODO("Not yet implemented") // 회원가입 전화번호 인증 체크 로직 필요
         } else if(request.servletPath == ApiRoute.RESET_PASSWORD) {
-
+            TODO("Not yet implemented") // 비밀번호 재설정 전화번호 인증 체크 로직 필요
         } else {
             request.getHeader(AUTHORIZATION)?.run {
                 if(startsWith("Bearer ")) {
